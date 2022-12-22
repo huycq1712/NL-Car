dd05cd8f15069038d7ab13bc622bffba39ace906
huycq1712 <huysk82000@gmail.com>
2022-12-22 23:51:13 +0700

    update build dataloader

--- datasets/build.py
+++ datasets/build.py
@@ -1,6 +1,10 @@
+import sys
+sys.path.append('/mnt/data/user_data/huycq/NL-Car')
 from torch.utils import data
 from nlcityflowdataset import NLCityFlowDataset
 from transforms import build_transforms
+
+from config.aicity_config import train_cfg as cfg
 
 
 def build_dataset(cfg, transforms, is_training):
@@ -22,3 +26,8 @@ def make_data_loader(cfg, is_training):
                                   pin_memory = True)
     
     return data_loader
+if __name__ == '__main__':
+    dataset = make_data_loader(cfg, True)
+    for i, data in enumerate(dataset):
+        print(data['car_noun'])
+    
