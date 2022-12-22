50e9c7a791d7ff2a0f546ba31bbee31f01161573
huycq1712 <huysk82000@gmail.com>
2022-12-22 23:50:50 +0700

    update transforms

--- datasets/transforms.py
+++ datasets/transforms.py
@@ -3,17 +3,41 @@ import torchvision.transforms as transforms
 
 
 def build_transforms(cfg, is_training):
-    
+  
     if is_training:
-        transforms = transforms.Compose([
-            transforms.RandomContrast(0.5, 1.5),
-            transforms.Resize(cfg['resize']),
-            transforms.ToTensor(),
-        ])
-    else:
-        transforms = transforms.Compose([
-            transforms.Resize(cfg['resize']),
+        transforms_frames = transforms.Compose([
+            #transforms.RandomContrast(0.5, 1.5),
+            transforms.Resize(cfg['im_size']),
             transforms.ToTensor(),
         ])
         
-    return transforms
+        transforms_motion = transforms.Compose([
+            #transforms.RandomContrast(0.5, 1.5),
+            transforms.Resize(cfg['im_size']),
+            transforms.ToTensor(),
+        ])
+        
+        transforms_crop = transforms.Compose([
+            #transforms.RandomContrast(0.5, 1.5),
+            transforms.Resize(cfg['im_size']),
+            transforms.ToTensor(),
+        ])
+    else:
+        transforms_frames = transforms.Compose([
+            transforms.Resize(cfg['im_size']),
+            transforms.ToTensor(),
+        ])
+        
+        transforms_motion = transforms.Compose([
+            #transforms.RandomContrast(0.5, 1.5),
+            transforms.Resize(cfg['im_size']),
+            transforms.ToTensor(),
+        ])
+        
+        transforms_crop = transforms.Compose([
+            #transforms.RandomContrast(0.5, 1.5),
+            transforms.Resize(cfg['im_size']),
+            transforms.ToTensor(),
+        ])
+        
+    return transforms_frames, transforms_motion, transforms_crop
